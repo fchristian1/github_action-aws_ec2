2a6410fbf44ad22376248197778b7d34b1317335
Christian Fröchtenicht <67558157+fchristian1@users.noreply.github.com>
2025-03-05 12:33:15 +0100

    1

--- html/script.js
+++ html/script.js
@@ -1,15 +1,15 @@
-const countText = document.getElementById("count")
-const count_dec = document.getElementById("count_dec")
-const count_inc = document.getElementById("count_inc")
+const countText = document.getElementById("count");
+const count_dec = document.getElementById("count_dec");
+const count_inc = document.getElementById("count_inc");
 
-let count = 0
+let count = 0;
 
 count_dec.addEventListener("click", function () {
-    count++;
-    countText.innerHtml = count;
-})
-count_inc.addEventListener("click", function () {
     count--;
-    countText.innerHtml = count;
+    countText.innerHTML = count;
+});
 
-})
+count_inc.addEventListener("click", function () {
+    count++;
+    countText.innerHTML = count;
+});
