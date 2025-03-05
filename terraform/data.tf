b99c17b4e9bdae98cd2092b16d813d9080f36f1b
Christian Fröchtenicht <67558157+fchristian1@users.noreply.github.com>
2025-03-05 10:02:50 +0100

    1

---
+++ terraform/data.tf
@@ -0,0 +1,32 @@
+data "external" "generate_ipfile-server" {
+  depends_on = [module.aws-instances-server]
+
+  program = ["bash", "-c", <<EOT
+    FILE="./type_ips"
+    > $FILE
+    for ip in ${join(" ", module.aws-instances-server[*].public_ip)}; do
+      echo "server:$ip" >> $FILE
+    done
+
+    echo "{\"status\": \"success\"}"
+  EOT
+  ]
+}
+data "external" "generate_inventory_first" {
+  depends_on = [data.external.generate_ipfile-server]
+
+  program = ["bash", "-c", <<EOT
+    INVENTORY_FILE="./ansible/inventory"
+    echo "[all:vars]" > $INVENTORY_FILE
+    echo "ansible_ssh_user=ubuntu" >> $INVENTORY_FILE
+    echo "ansible_ssh_private_key_file=./../my_key.pem" >> $INVENTORY_FILE
+    
+    echo "{\"status\": \"success\"}"
+  EOT
+  ]
+}
+data "external" "generate_inventory-sec" {
+  depends_on = [data.external.generate_inventory_first]
+
+  program = ["bash", "./generate_inventory.sh"]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+module "key_pair" {
+  source   = "./modules/aws_key_pair"
+  key_path = "./my_key.pem"
+  key_name = "my_aws_key"
+}
+module "sg" {
+  source = "./modules/aws_sg"
+  sg_rules = {
+    "ssh" = {
+      ingress = [
+        {
+          from_port   = 22
+          to_port     = 22
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    },
+    "http" = {
+      ingress = [
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    },
+    https = {
+      ingress = [{
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }]
+    }
+  }
+}
+
+module "aws-instances-webserver" {
+  source             = "./modules/aws_instance"
+  instance_name      = "${var.projekt_name}-server-${count.index}"
+  system_type        = "server"
+  count              = 1
+  key_name           = module.key_pair.name
+  security_group_ids = values(module.sg.sg_ids)
+  providers = {
+    aws = aws
+  }
+  depends_on = [module.key_pair, module.sg]
+}
