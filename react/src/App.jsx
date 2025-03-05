247831e7e4a90cf999c8b07c699f4e57f6b1a84a
Christian Fröchtenicht <67558157+fchristian1@users.noreply.github.com>
2025-03-05 11:49:12 +0000

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/counter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <h2>on EC2 with Terraform and Ansible CICD by github actions</h2>
+      <a target='_blank' href="https://github.com/fchristian1/github_action-aws_ec2">Github-Link</a>
+      <div className="card">
+        <Counter></Counter>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ react/src/components/counter.jsx
@@ -0,0 +1,14 @@
+import react, { useState } from 'react'
+
+function Counter() {
+    const [counter, setCounter] = useState(0);
+    return (<>
+        <p>
+            <button onClick={()=>setCounter(counter-1)}>-</button>
+            <button>{counter}</button>
+            <button onClick={()=>setCounter(counter+1)}>+</button>
+        </p>
+    </>)
+}
+
+export default Counter
