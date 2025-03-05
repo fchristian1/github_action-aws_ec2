de7256d05237898026a19669aa050bc5abe3afcc
Christian Fröchtenicht <67558157+fchristian1@users.noreply.github.com>
2025-03-05 10:31:43 +0100

    add varaible in terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "Region to deploy the instance"
+  type        = string
+  default     = "eu-central-1"
+}
+variable "projekt_name" {
+  description = "Name of the project"
+  type        = string
+
+}
